139336a8abb691c12bc2fb0a55fcf7c54869927f
Bakanyka <bakayaroshka@gmail.com>
2021-04-08 11:30:42 +0300

    Create default breeze app and modify index page to show bug

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,39 @@
+<template>
+<guest>
+    <div>
+        <div class="mt-2 text-right">
+            <label class="inline-flex items-center leading-none">
+                <input v-model="checkBox" type="checkbox" class="mr-2" />
+                <span>Test checkbox</span>
+            </label>
+        </div>
+        <div>
+            <div v-for="value in someData" :key="value">
+                {{value}}
+            </div>
+        </div>
+    </div>
+</guest>
+</template>
+
+<script>
+    import Guest from "../Layouts/Guest";
+    import {ref, watch} from "vue";
+    import { Inertia } from '@inertiajs/inertia';
+    export default {
+        components: {Guest},
+        props: {
+            someData: {
+                type: Array,
+                required: true,
+            }
+        },
+        setup() {
+            const checkBox = ref(false);
+            watch(checkBox, (value) => {
+                Inertia.reload({ data: { test: value ? 1 : 0 } });
+            });
+            return {checkBox}
+        }
+    }
+</script>
